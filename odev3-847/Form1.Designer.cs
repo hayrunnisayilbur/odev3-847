537b85c5e7512d6504bd8a9037e33fdf25ba9182
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2023-10-17 14:50:33 +0300

    Add files via upload

---
+++ odev3-847/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace odev3_847
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnKontrol = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(39, 91);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(213, 163);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "uzantı türü";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rb2);
+            this.groupBox2.Controls.Add(this.rb1);
+            this.groupBox2.Location = new System.Drawing.Point(327, 91);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(214, 163);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "dosya türü";
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Location = new System.Drawing.Point(6, 34);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(89, 17);
+            this.rb1.TabIndex = 0;
+            this.rb1.TabStop = true;
+            this.rb1.Text = "video dosyası";
+            this.rb1.UseVisualStyleBackColor = true;
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Location = new System.Drawing.Point(6, 57);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(87, 17);
+            this.rb2.TabIndex = 1;
+            this.rb2.TabStop = true;
+            this.rb2.Text = "resim dosyası";
+            this.rb2.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "MP4",
+            "JPG",
+            "MOV",
+            "PNG"});
+            this.listBox1.Location = new System.Drawing.Point(6, 34);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(147, 108);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btnKontrol
+            // 
+            this.btnKontrol.Location = new System.Drawing.Point(157, 270);
+            this.btnKontrol.Name = "btnKontrol";
+            this.btnKontrol.Size = new System.Drawing.Size(197, 42);
+            this.btnKontrol.TabIndex = 2;
+            this.btnKontrol.Text = "kontrol et";
+            this.btnKontrol.UseVisualStyleBackColor = true;
+            this.btnKontrol.Click += new System.EventHandler(this.btnKontrol_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnKontrol);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.Button btnKontrol;
+    }
+}
+
---
+++ odev3-847/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odev3_847
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnKontrol_Click(object sender, EventArgs e)
+        {
+            string secim;
+            bool cevap1, cevap2;
+            secim = listBox1.SelectedItems.ToString();
+            cevap1 = rb1.Checked;
+            cevap2 = rb2.Checked;
+            if(cevap1 == true && ( secim == "MP4" || secim == "MOV"))
+            {
+                MessageBox.Show("cevabınız doğru");
+            }
+            if(cevap1 == false && (secim == "MP4" || secim  == "MOV"))
+            {
+                MessageBox.Show("cevabınız yanlış");
+            }
+            if(cevap2 == true && (secim == "JPG" || secim == "PNG"))
+            {
+                MessageBox.Show("cevabınız doğru");
+            }
+            if(cevap2 == false && (secim == "JPG" || secim == "PNG"))
+            {
+                MessageBox.Show("cevabınız yanlış");
+            }
+        }
+    }
+}
